93fed5991e90f44a40a4793dd001f8b29616720a
mrshnra <mrshnra@gmail.com>
2015-10-11 11:47:00 -0400

    Update fib.py

--- fib.py
+++ fib.py
@@ -4,7 +4,13 @@ def fib_loop(n):
    
     It uses for loop to calculate it.
     '''
-
+    fib = [1,1]
+    
+    for i in range(n-2)
+        x= fib[i]+fib[i-1]
+        fib.append(x)
+        
+    return fib[n-1]
     
 def fib_recursion(n):
     '''
@@ -12,7 +18,15 @@ def fib_recursion(n):
    
     It uses for loop to calculate it.
     '''
-
+    fib1=1
+    fib2=1
+    
+    for i in range(n-2):
+        fib3 = fib2 + fib1
+        fib1 = fib2
+        fib2 = fib3
+        
+    return fib3
 
 print(fib_loop(8))
 print(fib_recursion(13))
